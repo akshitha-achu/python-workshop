d27d2cb66c030cf2ee7db96850ffa30b85a03305
akshitha-achu <135851338+akshitha-achu@users.noreply.github.com>
2025-05-21 14:30:20 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,18 @@
+class Person:
+       def owner(item):
+        print(item)
+if __name__=="__main__":
+    person=str(input("who are you?"))
+    if person=="owner":
+        Person.owner(person)
+    else: 
+        print("invalid person")
+    
+    
+    
+    
+    
+    
+    
+    
+    
